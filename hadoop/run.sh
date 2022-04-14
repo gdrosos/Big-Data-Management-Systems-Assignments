7cea30b6f87bf965e49e676cbefb12b1ff520324
gliargovas <56384743+gliargovas@users.noreply.github.com>
2022-04-14 16:51:22 +0300

    Update run.sh

--- hadoop/run.sh
+++ hadoop/run.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-
-
-# hadoop dfs -copyFromLocal /Users/gdrosos/Desktop/Damianos/HadoopAssignment /user/gdrosos/HadoopAssignment/
-# hadoop fs -mkdir /user/gdrosos/HadoopAssignment/input
-#  hadoop fs -mkdir /user/gdrosos/HadoopAssignment/output
-# hadoop dfs -copyFromLocal /Users/gdrosos/Desktop/Damianos/Big-Data-Management-Systems-Assignments/hadoop/points.txt /user/gdrosos/HadoopAssignment/input/data.txt
-# hadoop dfs -appendToFile /Users/gdrosos/Desktop/Damianos/Big-Data-Management-Systems-Assignments/hadoop/points.txt /user/gdrosos/HadoopAssignment/input/data.txt
-
-
-hadoop  jar /opt/homebrew/Cellar/hadoop/3.3.2/libexec/share/hadoop/tools/lib/hadoop-streaming-3.3.2.jar \
- -mapper /user/gdrosos/HadoopAssignment/mapper.py\
-  -reducer /user/gdrosos/HadoopAssignment/reducer.py \
--input /user/gdrosos/HadoopAssignment/input/data.txt \
-  -output /user/gdrosos/HadoopAssignment/output/a
+bin/hadoop jar share/hadoop/tools/lib/hadoop-streaming-3.2.3.jar \
+-file mapper.py \
+-file reducer.py \
+-mapper "python mapper.py" \
+-reducer "python reducer.py" \
+-input input/points.txt \
+-output output
